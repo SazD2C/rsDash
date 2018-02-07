8d945e2bd7bea2a902743aad563d4fc54c35afbf
SazD2C <SazD2C@protonmail.com>
2018-02-06 16:28:02 -0800

    Input labels added

--- newuser.php
+++ newuser.php
@@ -86,23 +86,57 @@
   <br /><br /><br />
 <p class="title"> Add a user</p>
 <form method="post" style="text-align: center;">
-  <input type="text" name="username" style="width: 400px; padding: 3px;" placeholder="username"><br /><br />
-  <input type="text" name="joindate" style="width: 400px; padding: 3px;" placeholder="joindate"><br /><br />
-  <input type="text" name="age" style="width: 400px; padding: 3px;" placeholder="age"><br /><br />
-  <input type="text" name="languages" style="width: 400px; padding: 3px;" placeholder="languages"><br /><br />
-  <input type="text" name="oldname" style="width: 400px; padding: 3px;" placeholder="oldname"><br /><br />
-  <input type="text" name="hunting" style="width: 400px; padding: 3px;" placeholder="hunting"><br /><br />
-  <input type="text" name="frontwebdev" style="width: 400px; padding: 3px;" placeholder="frontwebdev"><br /><br />
-  <input type="text" name="backwebdev" style="width: 400px; padding: 3px;" placeholder="backwebdev"><br /><br />
-  <input type="text" name="writing" style="width: 400px; padding: 3px;" placeholder="writing"><br /><br />
-  <input type="text" name="programming" style="width: 400px; padding: 3px;" placeholder="programming"><br /><br />
-  <input type="text" name="art" style="width: 400px; padding: 3px;" placeholder="art"><br /><br />
-  <input type="text" name="se" style="width: 400px; padding: 3px;" placeholder="se"><br /><br />
-  <input type="text" name="smm" style="width: 400px; padding: 3px;" placeholder="smm"><br /><br />
-  <input type="text" name="pentesting" style="width: 400px; padding: 3px;" placeholder="pentesting"><br /><br />
-  <input type="text" name="timezone" style="width: 400px; padding: 3px;" placeholder="timezone"><br /><br />
-  <input type="text" name="availability" style="width: 400px; padding: 3px;" placeholder="availability"><br /><br />
-  <input type="text" name="reliability" style="width: 400px; padding: 3px;" placeholder="reliability"><br /><br />
+<table>
+<tr>
+  <td>Username</td><td><input type="text" name="username" style="width: 400px; padding: 3px;" placeholder="username"><br /><br /><td>
+</tr>
+  <td>Join year</td><td><input type="text" name="joindate" style="width: 400px; padding: 3px;" placeholder="joindate"><br /><br />
+<tr>
+  <td>Age</td><td><input type="text" name="age" style="width: 400px; padding: 3px;" placeholder="age"><br /><br />
+</tr>
+<tr>
+  <td>Languages</td><td><input type="text" name="languages" style="width: 400px; padding: 3px;" placeholder="languages"><br /><br />
+</tr>
+<tr>
+  <td>Old Name</td><td><input type="text" name="oldname" style="width: 400px; padding: 3px;" placeholder="oldname"><br /><br />
+</tr>
+<tr>
+  <td>Hunting skill</td><td><input type="text" name="hunting" style="width: 400px; padding: 3px;" placeholder="hunting"><br /><br />
+</tr>
+<tr>
+  <td>Front web dev skill</td><td><input type="text" name="frontwebdev" style="width: 400px; padding: 3px;" placeholder="frontwebdev"><br /><br />
+</tr>
+<tr>
+  <td>Back web dev skill</td><td><input type="text" name="backwebdev" style="width: 400px; padding: 3px;" placeholder="backwebdev"><br /><br />
+</tr>
+<tr>
+  <td>Writing</td><td><input type="text" name="writing" style="width: 400px; padding: 3px;" placeholder="writing"><br /><br />
+</tr>
+<tr>
+  <td>Programming</td><td><input type="text" name="programming" style="width: 400px; padding: 3px;" placeholder="programming"><br /><br />
+</tr>
+<tr>
+  <td>Art</td><td><input type="text" name="art" style="width: 400px; padding: 3px;" placeholder="art"><br /><br />
+</tr>
+<tr>
+  <td>Social Engineering</td><td><input type="text" name="se" style="width: 400px; padding: 3px;" placeholder="se"><br /><br />
+</tr>
+<tr>
+  <td>Social Media Management</td><td><input type="text" name="smm" style="width: 400px; padding: 3px;" placeholder="smm"><br /><br />
+</tr>
+<tr>
+  <td>Pen Testing</td><td><input type="text" name="pentesting" style="width: 400px; padding: 3px;" placeholder="pentesting"><br /><br />
+</tr>
+<tr>
+  <td>Timezone</td><td><input type="text" name="timezone" style="width: 400px; padding: 3px;" placeholder="timezone"><br /><br />
+</tr>
+<tr>
+  <td>Availability</td><td><input type="text" name="availability" style="width: 400px; padding: 3px;" placeholder="availability"><br /><br />
+</tr>
+<tr>
+  <td>Reliability</td><td><input type="text" name="reliability" style="width: 400px; padding: 3px;" placeholder="reliability"><br /><br />
+</tr>
+</table>
   <input type="submit" name="submit" value="Submit" id="submit"/>
 </form>
 </body>
