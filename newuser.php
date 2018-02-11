dcd2575cadafa401f30d81b5df47a87c10b5ee12
SazD2C <SazD2C@protonmail.com>
2018-02-10 23:58:49 -0800

    New user fix

--- newuser.php
+++ newuser.php
@@ -30,6 +30,7 @@
   $timezone = $_POST['timezone'];
   $availability = $_POST['availability'];
   $reliability = $_POST['reliability'];
+  $avatar = $_POST['avatar'];
   if ($age == 0 or $age == '')  {
     $age = NULL;
   }
@@ -75,8 +76,11 @@
   if ($reliability == 0 or $reliability == '')  {
     $reliability = NULL;
   }
+  if ($avatar == 0 or $avatar == '')  {
+    $avatar = NULL;
+  }
 
-  $sql = "INSERT INTO userinfo (username, joindate, age, languages, oldname, hunting, frontwebdev, backwebdev, writing, programming, art, se, smm, pentesting, timezone, availability, reliability) VALUES ('" . $username . "', '" . $joindate . "', '" . $age . "', '" . $languages . "', '" . $oldname . "', '" . $hunting . "', '" . $frontwebdev . "', '" . $backwebdev . "', '" . $writing . "', '" . $programming . "', '" . $art . "', '" . $se . "', '" . $smm . "', '" . $pentesting . "', '" . $timezone . "', '" . $availability . "', '" . $reliability . "';";
+  $sql = "INSERT INTO userinfo (username, joindate, age, languages, oldname, hunting, frontwebdev, backwebdev, writing, programming, art, se, smm, pentesting, timezone, availability, reliability, avatar) VALUES ('" . $username . "', '" . $joindate . "', '" . $age . "', '" . $languages . "', '" . $oldname . "', '" . $hunting . "', '" . $frontwebdev . "', '" . $backwebdev . "', '" . $writing . "', '" . $programming . "', '" . $art . "', '" . $se . "', '" . $smm . "', '" . $pentesting . "', '" . $timezone . "', '" . $availability . "', '" . $reliability . "', '" . $avatar . "';";
   mysqli_query($connection,$sql);
   }
   mysqli_close($connection);
@@ -88,53 +92,56 @@
 <form method="post" style="text-align: center;">
 <table>
 <tr>
-  <td>Username</td><td><input type="text" name="username" style="width: 400px; padding: 3px;" placeholder="username"><br /><br /><td>
+  <td>Username</td><td><input type="text" name="username" style="width: 400px; padding: 3px;" placeholder="Required"><br /><br /><td>
 </tr>
-  <td>Join year</td><td><input type="text" name="joindate" style="width: 400px; padding: 3px;" placeholder="joindate"><br /><br />
+  <td>Join year</td><td><input type="text" name="joindate" style="width: 400px; padding: 3px;" placeholder="Optional"><br /><br />
 <tr>
-  <td>Age</td><td><input type="text" name="age" style="width: 400px; padding: 3px;" placeholder="age"><br /><br />
-</tr>
-<tr>
-  <td>Languages</td><td><input type="text" name="languages" style="width: 400px; padding: 3px;" placeholder="languages"><br /><br />
+  <td>Age</td><td><input type="text" name="age" style="width: 400px; padding: 3px;" placeholder="Optional"><br /><br />
 </tr>
 <tr>
-  <td>Old Name</td><td><input type="text" name="oldname" style="width: 400px; padding: 3px;" placeholder="oldname"><br /><br />
+  <td>Languages</td><td><input type="text" name="languages" style="width: 400px; padding: 3px;" placeholder="Optional"><br /><br />
 </tr>
 <tr>
-  <td>Hunting skill</td><td><input type="text" name="hunting" style="width: 400px; padding: 3px;" placeholder="hunting"><br /><br />
+  <td>Old Name</td><td><input type="text" name="oldname" style="width: 400px; padding: 3px;" placeholder="Optional"><br /><br />
 </tr>
 <tr>
-  <td>Front web dev skill</td><td><input type="text" name="frontwebdev" style="width: 400px; padding: 3px;" placeholder="frontwebdev"><br /><br />
+  <td>Hunting skill</td><td><input type="text" name="hunting" style="width: 400px; padding: 3px;" placeholder="Optional"><br /><br />
 </tr>
 <tr>
-  <td>Back web dev skill</td><td><input type="text" name="backwebdev" style="width: 400px; padding: 3px;" placeholder="backwebdev"><br /><br />
+  <td>Front web dev skill</td><td><input type="text" name="frontwebdev" style="width: 400px; padding: 3px;" placeholder="Optional"><br /><br />
 </tr>
 <tr>
-  <td>Writing</td><td><input type="text" name="writing" style="width: 400px; padding: 3px;" placeholder="writing"><br /><br />
+  <td>Back web dev skill</td><td><input type="text" name="backwebdev" style="width: 400px; padding: 3px;" placeholder="Optional"><br /><br />
 </tr>
 <tr>
-  <td>Programming</td><td><input type="text" name="programming" style="width: 400px; padding: 3px;" placeholder="programming"><br /><br />
+  <td>Writing</td><td><input type="text" name="writing" style="width: 400px; padding: 3px;" placeholder="Optional"><br /><br />
 </tr>
 <tr>
-  <td>Art</td><td><input type="text" name="art" style="width: 400px; padding: 3px;" placeholder="art"><br /><br />
+  <td>Programming</td><td><input type="text" name="programming" style="width: 400px; padding: 3px;" placeholder="Optional"><br /><br />
 </tr>
 <tr>
-  <td>Social Engineering</td><td><input type="text" name="se" style="width: 400px; padding: 3px;" placeholder="se"><br /><br />
+  <td>Art</td><td><input type="text" name="art" style="width: 400px; padding: 3px;" placeholder="Optional"><br /><br />
 </tr>
 <tr>
-  <td>Social Media Management</td><td><input type="text" name="smm" style="width: 400px; padding: 3px;" placeholder="smm"><br /><br />
+  <td>Social Engineering</td><td><input type="text" name="se" style="width: 400px; padding: 3px;" placeholder="Optional"><br /><br />
 </tr>
 <tr>
-  <td>Pen Testing</td><td><input type="text" name="pentesting" style="width: 400px; padding: 3px;" placeholder="pentesting"><br /><br />
+  <td>Social Media Management</td><td><input type="text" name="smm" style="width: 400px; padding: 3px;" placeholder="Optional"><br /><br />
 </tr>
 <tr>
-  <td>Timezone</td><td><input type="text" name="timezone" style="width: 400px; padding: 3px;" placeholder="timezone"><br /><br />
+  <td>Pen Testing</td><td><input type="text" name="pentesting" style="width: 400px; padding: 3px;" placeholder="Optional"><br /><br />
 </tr>
 <tr>
-  <td>Availability</td><td><input type="text" name="availability" style="width: 400px; padding: 3px;" placeholder="availability"><br /><br />
+  <td>Timezone</td><td><input type="text" name="timezone" style="width: 400px; padding: 3px;" placeholder="Optional"><br /><br />
 </tr>
 <tr>
-  <td>Reliability</td><td><input type="text" name="reliability" style="width: 400px; padding: 3px;" placeholder="reliability"><br /><br />
+  <td>Availability</td><td><input type="text" name="availability" style="width: 400px; padding: 3px;" placeholder="Required"><br /><br />
+</tr>
+<tr>
+  <td>Reliability</td><td><input type="text" name="reliability" style="width: 400px; padding: 3px;" placeholder="Required"><br /><br />
+</tr>
+<tr>
+  <td>Url to avatar</td><td><input type="text" name="avatar" style="width: 400px; padding: 3px;" placeholder="Optional"><br /><br />
 </tr>
 </table>
   <input type="submit" name="submit" value="Submit" id="submit"/>
